971b04e34cd3d19763b61c008e63ab3f56141a56
Tak H Kim <takhyun818@gmail.com>
2018-07-28 07:46:18 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -3,7 +3,7 @@ require_relative"../lib/display_board.rb"
 describe "#display_board in 'lib/display_board.rb" do
   context 'various game situations' do
     it 'prints a blank board when the board array is empty' do
-      board = [" "," "," "," "," "," "," "," "," "]
+      board = [" ","x"," "," "," "," "," "," "," "]
 
       output = capture_puts{ display_board(board) }
       rows = output.split("\n")
